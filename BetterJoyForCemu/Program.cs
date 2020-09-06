5b4238fb569f5dfdb33ad2257d19e48c7bdb33f5
Juan Carlos Solano Flórez <jsolanof.cr@gmail.com>
2020-09-06 08:57:08 -0600

    Add Selector

--- BetterJoyForCemu/Program.cs
+++ BetterJoyForCemu/Program.cs
@@ -109,6 +109,15 @@ namespace BetterJoyForCemu {
         }
 
         public void CheckForNewControllers() {
+
+            // Add instance of 3rdParty type selector
+            Selector3rdPartyType selector = new Selector3rdPartyType();
+            ushort[] custom_calib = null;
+            ushort[] custom_calib2 = null;
+            if (form.nonOriginal) {
+                selector.Load();
+            }
+
             // move all code for initializing devices here and well as the initial code from Start()
             bool isLeft = false;
             IntPtr ptr = HIDapi.hid_enumerate(0x0, 0x0);
@@ -126,9 +135,12 @@ namespace BetterJoyForCemu {
                     continue;
                 }*/
 
-                if (form.nonOriginal) {
-                    enumerate.product_id = product_pro;
+                if (form.nonOriginal && enumerate.product_string != "Nintendo RVL-CNT-01") {
+                    enumerate.product_id = selector.GetProductId(enumerate.serial_number); // Check if this is a known controller
+                    custom_calib = selector.GetCalib(enumerate.serial_number);
+                    custom_calib2 = selector.GetCalib2(enumerate.serial_number);
                 }
+
 
                 bool validController = (enumerate.product_id == product_l || enumerate.product_id == product_r ||
                                         enumerate.product_id == product_pro || enumerate.product_id == product_snes) && enumerate.vendor_id == vendor_id;
